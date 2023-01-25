2387840fd8c65eec26a84ef30ab3a0d4df4d2913
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-25 09:04:54 +0000

    [add]controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get root to: "homes#top"
+  end
   devise_for :customers,skip: [:passwords],controllers:{
     registrations: "public/registrations",
     sessions: 'public/sessions'
