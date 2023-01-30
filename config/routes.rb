ade80c1d9987dd1ce6d91b412801c00d986f52cf
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-01-30 12:50:36 +0000

    [update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
     get root to: "homes#top"
     get 'homes/about', as: :about
     
-    get '/customers/sign_up' => 'registrations#new' ,as: :new_customer
+    get 'customers/sign_up' => 'registrations#new' 
+    get 'customers' => 'registrations#create' 
     
-    get '/customers/sign_in' => 'sessions#new',as: :new_sesstion
+    get 'customers/sign_in' => 'sessions#new'
+    get '/customers/sign_out' => 'sessions#destroy'
     
-    get 'customers/my_page' => 'customers#show', as: :customer
-    get 'customers/information/edit' => 'customers#edit',as: :edit_customer
-    get 'customers/unsubscribe' => 'customers#confirm',as: :confirm_customer
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    get 'customers/unsubscribe' => 'customers#confirm'
     
     resources :orders, only: [:new,:confirm,:complete,:index,:show] do
       collection do
@@ -19,15 +21,21 @@ Rails.application.routes.draw do
         get 'complete'
       end
     end
-    resources :cart_items, only: [:index]
-    #resources :customers, only: [:show,:edit,:confirm] ->これは上に個別でルートの指定をした。
+    
+    resources :cart_items, only: [:index,:destroy] do
+       collection do
+         get 'destroy_all'
+       end
+     end
+    
     resources :items, only: [:index,:show]
     resources :addresses,only: [:index,:edit,:create,:update,:destroy]
   end
   
+  
   namespace :admin do
     get root to: "homes#top"
-    resources :sessions, only: [:new,:create,:destroy]
+    
     resources :orders, only: [:show]
     resources :customers, only: [:index,:show,:edit]
     resources :items, only: [:index,:new,:show,:edit]
