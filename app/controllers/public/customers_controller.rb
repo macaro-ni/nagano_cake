37c7c158afbb6f10132e3b1b99081199c793bcec
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-02-09 09:03:26 +0000

    [add]会員情報編集ページ

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,7 +5,15 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer=current_customer
   end
+
+  def update
+    customer=current_customer
+    customer.update(customer_params)
+    redirect_to customers_my_page_path(customer.id)
+  end
+
 
   def confirm
 
@@ -20,5 +28,9 @@ class Public::CustomersController < ApplicationController
     redirect_to root_path
   end
 
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:postal_code,:address,:telephone_number)
+  end
 
 end
