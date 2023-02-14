e28fcd02f91fc1834472c3f61809acd200326be5
macaro-ni <119795137+macaro-ni@users.noreply.github.com>
2023-02-14 16:27:11 +0000

    [add]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,18 @@
 class Public::OrdersController < ApplicationController
-  
-  
+
+
   def new
     @order=Order.new
+  end
+
+  def create
+    order=Order.new(order_params)
+    order.customer_id=current_customer.id
+    order.shipping_cost=800
+    #合計を、とりあえず０で入れてる
+    order.total_payment=0+order.shipping_cost
+    order.save
+    redirect_to confirm_orders_path
   end
 
   def confirm
@@ -16,4 +26,12 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+
+    private
+  def order_params
+      params.require(:order).permit(:postal_code, :address, :name, :payment_method)
+  end
+
+
 end
